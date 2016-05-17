bf4930b226637106a83b60198534f100848c7d1a
Tucker Connelly <web@tuckerconnelly.com>
2016-05-17 13:57:58 -0400

    mergeArrays on both css and style

--- src/plugins/mergeArray.js
+++ src/plugins/mergeArray.js
@@ -1,18 +1,22 @@
 import React from 'react'
 
+function reduceStyle(style) {
+  if (typeof style !== 'object') return style
+  if (!Array.isArray(style)) return style
+  return style.reduce(
+    (styleAccumulator, currentStyle) =>
+      currentStyle ? ({ ...styleAccumulator, ...currentStyle }) : styleAccumulator,
+    {}
+  )
+}
+
 export default element => {
   const { props } = element
-  const { css } = props
+  const { css, style } = props
+  if (typeof css !== 'object' || !Array.isArray(css)) return element
 
-  if (typeof css === 'object' && Array.isArray(css)) {
-    const newCSS = css.reduce(
-      (cssAccumulator, currentStyle) =>
-        currentStyle ? ({ ...cssAccumulator, ...currentStyle }) : cssAccumulator,
-      {}
-    )
+  const newCSS = reduceStyle(css)
+  const newStyle = reduceStyle(style)
 
-    return React.cloneElement(element, { ...props, css: newCSS })
-  }
-
-  return element
+  return React.cloneElement(element, { ...props, css: newCSS, style: newStyle })
 }
