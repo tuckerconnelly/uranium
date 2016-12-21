f45a33ce99f9a99cd682af99266c319f7663ba02
Tucker Connelly <web@tuckerconnelly.com>
2016-12-21 09:06:54 -0500

    Set matchMediaMock and console.warn if it's not set

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import { Component } from 'react'
 import { DOMProperty } from 'react/lib/ReactInjection'
 
+import matchMediaMock from './matchMediaMock'
 import resolveStyles from './resolveStyles'
 
 DOMProperty.injectDOMPropertyConfig({
   isCustomAttribute: attributeName => attributeName === 'css',
 })
+
+if (!global.matchMedia) {
+  global.matchMedia = matchMediaMock
+
+  if (!global.__exponent) {
+    console.warn( // eslint-disable-line no-console
+      'global.matchMedia not found. Uranium has mocked it for you. ' +
+      'To get rid of this warning, in your index.ios.js or index.android.js ' +
+      'set global.matchMedia to react-native-match-media or to Uranium\'s `matchMediaMock`. ' +
+      'See the Uranium docs for details.'
+    )
+  }
+}
 
 export { default as animate } from './animate'
 export { default as matchMediaMock } from './matchMediaMock'
